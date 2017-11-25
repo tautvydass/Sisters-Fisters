d587891fba06c6b5908d5b9e896c04a1cb8b57d3
Tautvydas <31304974+tautvydass@users.noreply.github.com>
2017-11-25 15:42:58 +0200

    Splash/character selection

---
+++ Assets/Scripts/SplashInput.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashInput : MonoBehaviour
+{
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+			Application.Quit();
+		else if(Input.anyKeyDown)
+			Load();
+	}
+
+	private void Load() =>
+		SceneManager.LoadScene("Character Selection");
+}
