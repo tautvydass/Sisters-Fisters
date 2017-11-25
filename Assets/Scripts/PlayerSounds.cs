774a0a04f841d8a19c957be40c097728e77fb3a2
Tautvydas <31304974+tautvydass@users.noreply.github.com>
2017-11-25 23:42:44 +0200

    Particles and Sounds

---
+++ Assets/Scripts/PlayerSounds.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Sisters Fisters/Sounds", fileName = "New Player Sounds")]
+public class PlayerSounds : ScriptableObject
+{
+	public AudioClip selection;
+	public AudioClip jump;
+	public AudioClip hit;
+	public AudioClip getHit;
+}
