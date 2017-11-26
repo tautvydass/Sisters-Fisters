f610b6f0d04a2b591e1445598fe91625490e863c
JCMVenom <33966318+JCMVenom@users.noreply.github.com>
2017-11-26 09:05:27 +0200

    Add files via upload

---
+++ Assets/Vaidas/Scripts/MoonRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,0,0.1f); // cant rotate negative
+		if (transform.eulerAngles.z>=70f){
+			transform.eulerAngles=Vector3.zero;
+			
+		}
+	}
+}
