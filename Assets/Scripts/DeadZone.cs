1323f6f30e0bbece8f6d9b3951a75613e2527b22
Justas <glodjus@gmail.com>
2017-11-26 03:42:05 +0200

    Adding dead zone and spawning manager script

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        var player = collision.collider.gameObject.GetComponent<Player>();
+        if (player)
+        {
+            player.Lives--;
+            if (player.Lives > 0)
+            {
+                player.Health = 0;
+                SpawningManager.Spawn(player);
+            }
+            else
+            {
+                Destroy(player);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawningManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class SpawningManager : MonoBehaviour
+{
+    public Transform[] SpawnPoints;
+
+	void Start ()
+    {
+        SpawnPoints = transform.GetComponentsInChildren<Transform>().Where(t => t.gameObject != this).ToArray();
+    }
+
+    public static Player Spawn(Player player)
+    {
+        var go = GameObject.FindGameObjectWithTag("SpawningManager");
+        if (go)
+        {
+            var manager = go.GetComponent<SpawningManager>();
+            return manager.SpawnInternal(player);
+        }
+        else
+        {
+            Debug.LogError("Cannot spawn players because spawning manager does not have tag 'SpawningManager' or does not exist.");
+            return player;
+        }
+    }
+
+    public Player SpawnInternal(Player player)
+    {
+        player.transform.position = GetRandomPos();
+        return player;
+    }
+
+    public Vector3 GetRandomPos()
+    {
+        var ran = Random.Range(0, SpawnPoints.Length - 1);
+        return SpawnPoints[ran].position;
+    }
+}
